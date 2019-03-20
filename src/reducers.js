b3a6125900231b542dc5fa2838799c08eed513ee
romgu <gmarca20@gmail.com>
2019-03-20 13:28:13 -0300

    developing react-redux

---
+++ src/reducers.js
@@ -0,0 +1,14 @@
+import { CHANGE_SEARCH_FIELD } from './constants.js';
+
+const initialState = {
+	searchField: ''
+}
+
+export const searchCats = (state=initialState, action={}) => {
+	switch(action.tye) {
+		case: CHANGE_SEARCH_FIELD:
+			return Object.assign({}, state, {searchField: action.payload})
+		default: 
+			return state;
+	}
+}
