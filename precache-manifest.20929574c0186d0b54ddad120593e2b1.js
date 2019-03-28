5c18a5e6ace8d7997a6677211e55b86e0c6b0d80
romgu <gmarca20@gmail.com>
2019-03-28 13:25:31 -0300

    Updates

---
+++ precache-manifest.20929574c0186d0b54ddad120593e2b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "66b7e1568d85b12baeb4",
+    "url": "/catfriends/static/js/runtime~main.2a0eb0af.js"
+  },
+  {
+    "revision": "2bf6be243053a6826577",
+    "url": "/catfriends/static/js/main.49161cf3.chunk.js"
+  },
+  {
+    "revision": "0cb94423efebe6afd604",
+    "url": "/catfriends/static/js/2.a86bef57.chunk.js"
+  },
+  {
+    "revision": "2bf6be243053a6826577",
+    "url": "/catfriends/static/css/main.3fc74ae3.chunk.css"
+  },
+  {
+    "revision": "0cb94423efebe6afd604",
+    "url": "/catfriends/static/css/2.051398c9.chunk.css"
+  },
+  {
+    "revision": "180286f438b863df2977246ab7909b75",
+    "url": "/catfriends/index.html"
+  }
+];
