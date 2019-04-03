61bef3aedc156649af3d3f6a00cb09526f263a14
romgu <gmarca20@gmail.com>
2019-04-03 13:18:17 -0300

    .

---
+++ src/components/Card.test.js
@@ -0,0 +1,9 @@
+import { shallow, mount, render} from 'enzyme';
+import React from 'react';
+import Card from './Card';
+
+
+it('expect to render card component', () => {
+	expect(shallow(<Card />).length).toEqual(1)
+	
+})
